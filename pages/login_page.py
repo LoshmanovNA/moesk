b0c16d0eeff9bd0d536b19bcfb646f78b63eedf0
Nik <nik_0704@bk.ru>
2019-12-06 22:30:56 +0300

    exclude data

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,19 +4,14 @@ from .locators import LoginPageLocators as LPL, RemindPasswordLocators as RPL
 
 class LoginPage(BaseCase):
     """Действия на странице логина"""
-    def login_existing_user(self, login, password):
+    def login_user(self, login, password):
         """Авторизация под существующим пользователем"""
         self.update_text(LPL.LOGIN_INPUT, login)
         self.update_text(LPL.PASS_INPUT, password)
         self.click(LPL.SUBMIT_BUTTON)
 
-    def restore_password(self):
-        """Восстановление пароля"""
-        # Делаем запрос на восстановление пароля
-        self.click(LPL.REMIND_PASS_BUTTON)
-        self.update_text(RPL.USER_EMAIL)
-        self.click(RPL.SEND_BUTTON)
-        # Идем в почтовый ящик, проверяем что письмо пришло и переходим по ссылке
+    def should_be_main_page_lk(self):
+
 
 
 
